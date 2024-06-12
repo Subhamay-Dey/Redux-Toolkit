ea5b8976ac10722d8c18e30c35eac8561e5cf16e
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-12 19:45:12 +0530

    Sucessfully completed my first react-redux-toolkit project used useAppSelector and useAppDispatch hooks and added Redux store and connected it to App component with counter functionality ;))

--- react_redux_toolkit-project1/src/App.tsx
+++ react_redux_toolkit-project1/src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useAppDispatch, useAppSelector } from './redux/hooks/hook';
+import { increment, decrement } from './redux/slice/counter';
+import MyComp from './components/component';
 function App() {
+
+  const count = useAppSelector((state) => state.counter)
+  const dispatch = useAppDispatch();
 
   return (
     <>
@@ -14,7 +19,13 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <h1>Count is {count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+
       </div>
+
+      <MyComp/>
       
     </>
   )
